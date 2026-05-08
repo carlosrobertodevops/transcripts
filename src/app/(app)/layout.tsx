5df815385af9518676411860eeb5af97b85e197c
“geniuscodemycombr” <geniuscodemy.com.br@gmail.com>
2026-05-08 13:05:20 -0300

    feat(ui): add fade+slide page transitions in app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation'
 import { cookies } from 'next/headers'
 import { Sidebar } from '@/components/app/sidebar'
 import { Header } from '@/components/app/header'
+import { PageTransition } from '@/components/providers/page-transition'
 
 async function getCurrentUser() {
   const cookieStore = await cookies()
@@ -36,7 +37,7 @@ export default async function AppLayout({
         <Header />
         <main className="flex-1 overflow-auto">
           <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-            {children}
+            <PageTransition>{children}</PageTransition>
           </div>
         </main>
       </div>
